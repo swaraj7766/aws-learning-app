f90d63ae422b6966b3c274f04d5113c3f3d0b164
swaraj kumar <swaraj.k@atop.com.tw>
2021-12-23 08:57:37 +0530

    theme change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import React from "react";
+import { useThemeSwitcher } from "react-css-theme-switcher";
+import { Card } from "primereact/card";
 
 function App() {
-  return <div className="App">aws-learning-app is Working !</div>;
+  const { switcher, themes, currentTheme, status } = useThemeSwitcher();
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+
+  if (status === "loading") {
+    return <div>Loading styles...</div>;
+  }
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((previous) => {
+      switcher({ theme: previous ? themes.light : themes.dark });
+      return !previous;
+    });
+  };
+  return (
+    <Card>
+      aws-learning-app is Working !<h2>Current theme: {currentTheme}</h2>
+      <button onClick={toggleDarkMode}>toggle theme</button>
+    </Card>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 import "./index.css";
 import App from "./App";
+import { ThemeSwitcherProvider } from "react-css-theme-switcher";
+
+const themes = {
+  light: "/light.css",
+  dark: "/dark.css",
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeSwitcherProvider defaultTheme="light" themeMap={themes}>
+      <App />
+    </ThemeSwitcherProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
